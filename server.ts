938b6ad5e8d126496b2ff10bae8ecdc84ca5a126
wilhelm <helm@hey.com>
2023-02-03 21:55:19 +1100

    recognise tags

---
+++ server.ts
@@ -0,0 +1,160 @@
+import {
+  createConnection,
+  TextDocuments,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+} from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+interface Settings {
+  projectPrefixes: Array<string>;
+}
+
+const defaultSettings: Settings = { projectPrefixes: [] };
+let globalSettings: Settings = defaultSettings;
+
+const connection = createConnection(ProposedFeatures.all);
+const documents = new TextDocuments(TextDocument);
+
+const documentSettings: Map<string, Thenable<Settings>> = new Map();
+
+let supportsConfiguration = false;
+let supportsWorkspaceFolder = false;
+let supportsRelatedInformation = false;
+
+connection.onInitialize((params: InitializeParams) => {
+  const capabilities = params.capabilities;
+  const workspace = capabilities.workspace;
+  const textDocument = capabilities.textDocument;
+
+  supportsConfiguration = !!workspace?.configuration;
+  supportsWorkspaceFolder = !!workspace?.workspaceFolders;
+  supportsRelatedInformation =
+    !!textDocument?.publishDiagnostics?.relatedInformation;
+
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
+
+  if (supportsWorkspaceFolder) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+      },
+    };
+  }
+
+  return result;
+});
+
+connection.onInitialized(() => {
+  if (supportsConfiguration) {
+    // Listen to config changes
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+});
+
+connection.onDidChangeConfiguration((change) => {
+  if (supportsConfiguration) {
+    documentSettings.clear();
+  } else {
+    // TODO: Validate this and send notification if cooked.
+    globalSettings = <Settings>(change.settings.lls || defaultSettings);
+  }
+
+  documents.all().forEach(identifyTickets);
+});
+
+function getDocumentSettings(resource: string): Thenable<Settings> {
+  if (!supportsConfiguration) {
+    return Promise.resolve(globalSettings);
+  }
+
+  let result = documentSettings.get(resource);
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "lls",
+    });
+
+    documentSettings.set(resource, result);
+  }
+
+  return result;
+}
+
+documents.onDidClose((e) => {
+  documentSettings.delete(e.document.uri);
+});
+
+documents.onDidChangeContent((change) => {
+  identifyTickets(change.document);
+});
+
+async function identifyTickets(textDocument: TextDocument): Promise<void> {
+  const settings = await getDocumentSettings(textDocument.uri);
+  if (settings.projectPrefixes.length === 0) {
+    return;
+  }
+
+  const text = textDocument.getText();
+  const diagnostics: Diagnostic[] = [];
+  settings.projectPrefixes.forEach((prefix) => {
+    Array.from(text.matchAll(new RegExp(`${prefix}-[0-9]*`, "g"))).forEach(
+      (m) => {
+        diagnostics.push({
+          source: "Linear",
+          message: m[0],
+          severity: DiagnosticSeverity.Hint,
+          range: {
+            start: textDocument.positionAt(m.index ?? 0),
+            end: textDocument.positionAt((m.index ?? 0) + m[0].length),
+          },
+        });
+      }
+    );
+  });
+
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onCompletion(
+  (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    return [
+      {
+        label: "MOB-1",
+        data: "MOB-1",
+        kind: CompletionItemKind.Reference,
+      },
+    ];
+  }
+);
+
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+  if (item.data === "MOB-1") {
+    item.detail = "Install the application";
+    item.documentation = "Details details";
+  }
+
+  return item;
+});
+
+documents.listen(connection);
+
+connection.listen();
